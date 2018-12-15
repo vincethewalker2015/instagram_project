ec58c86b43cedd2e689c287e418a322df796a6dd
vincentbwalker <vincethewalker@gmail.com>
2018-12-15 16:20:20 +0000

    Added users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'home/index'
   
   root to: 'home#index'
+  resources :users, only: [:show]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_15_155647) do
+ActiveRecord::Schema.define(version: 2018_12_15_160619) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2018_12_15_155647) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "name"
+    t.string "website"
+    t.text "bio"
+    t.integer "phone"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
